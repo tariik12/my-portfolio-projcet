67bb966add2f8e9fc913cfe3694c775b9ccb17fd
tarikul-islam <tariikul12@gmail.com>
2023-08-02 23:40:00 +0600

    project search system by technology done

--- src/Pages/AllProject/AllProject.jsx
+++ src/Pages/AllProject/AllProject.jsx
@@ -1,10 +1,10 @@
 import {  useEffect, useState } from "react";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 import 'react-tabs/style/react-tabs.css';
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
 import { Helmet } from "react-helmet";
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaSistrix } from "react-icons/fa";
 // import { GoProjectSymlink } from "react-icons/go";
 // import { ThemContext } from "../../Layout/Main";
 
@@ -15,11 +15,7 @@ const AllProject = () => {
     // const [projects, setprojects] = useState([])
   const [loading,setLoading] = useState(true)
 
-  const fullStacks = projects.filter(data => data.id === 'Full Stack')
-  const frontEnd = projects.filter(data => data.id === 'Front End' || data.id === 'React.js')
-  const Bootstraps = projects.filter(data => data.id === 'Bootstrap')
-  const HTMLCSS = projects.filter(data => data.id === 'HTML & CSS')
-  const JavaScripts = projects.filter(data => data.id === 'JavaScript')
+  const [searchText, setSearchText] = useState('')
 
     console.log(projects)
         useEffect(() =>{
@@ -32,6 +28,16 @@ const AllProject = () => {
             })
         },[])
 
+        const handleSearch =() =>{
+          setLoading(true)
+          fetch(`https://my-portfolio-server-psi.vercel.app/projectsSearch/${searchText}`)
+          .then(res =>res.json())
+          .then(data =>{
+              setLoading(false)
+             setProject(data)
+              console.log(data)
+          })
+        }
         if(loading){
           return  <progress className="progress w-56"></progress>
           }
@@ -49,65 +55,17 @@ const AllProject = () => {
       </Link>
     
             </div>
- <Tabs >
-      <TabList className='text-center bg-gradient-to-t from-cyan-500  to-indigo-400 my-3'>
-        <Tab >HTML & CSS </Tab>
-        <Tab>Bootstrap</Tab>
-        <Tab>Javascript</Tab>
-        <Tab>All Project</Tab>
-        {/* <Tab>Tailwind</Tab> */}
-        <Tab>Front End</Tab>
-        <Tab>Full Stack Project</Tab>
-        
-      </TabList>
-      <TabPanel>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4  mx-2 ">
-        {
-            HTMLCSS.map(project =><div  key={project.id} className=" overflow-hidden bg-opacity-80  pt-20 hover:pt-0 group " style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
-               <Link to={`/projectDetails/${project._id}`} className="relative"> <img  className="  flex opacity-10 mx-auto   hover:opacity-100 h-1/2  bg-blue-400 transition-all group-hover:w-full group-hover:h-full" src={project.image1} alt=""  title="click here for showing details"/>
-               <div>
-               <div className=" top-3  opacity-100 group-hover:opacity-0  text-cyan-300  ">
-                
-               <p className="text-xl w-full text-center font-bold">{project.projectName}</p>
-               <p className=" text-center my-3 border-b-2  w-full pb-2">Technology</p>
-               <Marquee>
-               <p className="inline text-black font-bold">{project.tec}</p>
-               </Marquee>
-               </div>
-               </div>
-                </Link>
-            </div>)
-        }
-            
-        </div>
 
-      </TabPanel>
-      <TabPanel>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-2 ">
-        {
-            Bootstraps.map(project =><div  key={project.id} className=" overflow-hidden  pt-20 hover:p-2 rounded-sm group " style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
-               <Link to={`/projectDetails/${project._id}`} className="relative"> <img  className="  flex opacity-10 mx-auto   hover:opacity-100 h-1/2  bg-blue-400 transition-all group-hover:w-full group-hover:h-full rounded-sm" src={project.image1} alt=""  title="click here for showing details"/>
-               <div>
-               <div className="   opacity-100 group-hover:opacity-0  text-cyan-300  ">
-                
-               <p className="   text-xl text-center font-bold">{project.projectName}</p>
-               <p className=" text-center my-3 border-b-2 pb-2">Technology</p>
-               <Marquee>
-               <p className="inline text-black font-bold">{project.tec}</p>
-               </Marquee>
-               </div>
-               </div>
-                </Link>
-            </div>)
-        }
-            
-        </div>
+            <div className="form-control md:w-1/3  absolute z-50  left-[400px] mx-10 bg-black rounded-2xl">
+                    <div className="">
+                        <input onChange={(event)=>setSearchText(event.target.value)} type="text" placeholder="Search by Technology such as react or javascript" className="input bg-transparent border-cyan-100 w-full pr-16 text-cyan-100"  required/>
+                        <button onClick={handleSearch} className="  bg-transparent hover:none absolute top-2 rounded-2xl right-0 rounded-l-none"><FaSistrix className="w-16 text-cyan-100 h-8"/></button>
+                    </div>
+                </div>
 
-      </TabPanel>
-      <TabPanel>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-2 ">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-2 mt-32">
         {
-            JavaScripts.map(project =><div  key={project.id} className=" overflow-hidden  pt-20 hover:pt-0 group " style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
+            projects.map(project =><div  key={project.id} className=" overflow-hidden  pt-20 hover:pt-0 group rounded-2xl" style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
                <Link to={`/projectDetails/${project._id}`} className="relative"> <img  className="  flex opacity-10 mx-auto   hover:opacity-100 h-1/2  bg-blue-400 transition-all group-hover:w-full group-hover:h-full" src={project.image1} alt=""  title="click here for showing details"/>
                <div>
                <div className="  opacity-100 group-hover:opacity-0  text-cyan-300  ">
@@ -123,78 +81,8 @@ const AllProject = () => {
             </div>)
         }
             
-        </div>
-
-      </TabPanel>
-      <TabPanel>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-2 ">
-        {
-            projects.map(project =><div  key={project.id} className=" overflow-hidden  pt-20 hover:pt-0 group " style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
-               <Link to={`/projectDetails/${project._id}`} className="relative"> <img  className="  flex opacity-10 mx-auto   hover:opacity-100 h-1/2  bg-blue-400 transition-all group-hover:w-full group-hover:h-full" src={project.image1} alt=""  title="click here for showing details"/>
-               <div>
-               <div className="  opacity-100 group-hover:opacity-0  text-cyan-300  ">
-                
-               <p className="   text-xl text-center font-bold">{project.projectName}</p>
-               <p className=" text-center my-3 border-b-2 pb-2">Technology</p>
-               <Marquee>
-               <p className="inline text-black font-bold">{project.tec}</p>
-               </Marquee>
-               </div>
-               </div>
-                </Link>
-            </div>)
-        }
-            
-        </div>
-
-      </TabPanel>
-      
-      <TabPanel>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-2 ">
-        {
-            frontEnd.map(project =><div  key={project.id} className=" overflow-hidden  pt-20 hover:pt-0 group " style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
-               <Link to={`/projectDetails/${project._id}`} className="relative"> <img  className="  flex opacity-10 mx-auto   hover:opacity-100 h-1/2  bg-blue-400 transition-all group-hover:w-full group-hover:h-full" src={project.image1} alt=""  title="click here for showing details"/>
-               <div>
-               <div className="  opacity-100 group-hover:opacity-0  text-cyan-300  ">
-                
-               <p className="   text-xl text-center font-bold">{project.projectName}</p>
-               <p className=" text-center my-3 border-b-2 pb-2">Technology</p>
-               <Marquee>
-               <p className="inline text-black font-bold">{project.tec}</p>
-               </Marquee>
-               </div>
-               </div>
-                </Link>
-            </div>)
-        }
-            
-        </div>
-
-      </TabPanel>
-      <TabPanel>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mx-2 ">
-        {
-            fullStacks.map(project =><div  key={project.id} className=" overflow-hidden  pt-20 hover:pt-0 group" style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
-               <Link to={`/projectDetails/${project._id}`} className="relative"> <img  className="  flex opacity-10 mx-auto   hover:opacity-100 h-1/2  bg-blue-400 transition-all group-hover:w-full group-hover:h-full" src={project.image1} alt=""  title="click here for showing details"/>
-               <div>
-               <div className="absolute top-3  opacity-100 group-hover:opacity-0  text-cyan-300  ">
-                
-               <p className="   text-xl text-center font-bold">{project.projectName}</p>
-               <p className=" text-center my-3 border-b-2 pb-2">Technology</p>
-               <Marquee>
-               <p className="inline text-black font-bold">{project.tec}</p>
-               </Marquee>
-               </div>
-               </div>
-                </Link>
-            </div>)
-        }
-            
-        </div>
-
-      </TabPanel>
-    </Tabs>
          <h1 className="text-center text-4xl animate-pulse mt-5">PortFolio Projects Updating</h1>
+       </div>
        </div>
     );
 };
--- src/Pages/Home/Project/Project.jsx
+++ src/Pages/Home/Project/Project.jsx
@@ -5,30 +5,58 @@ import Header from "../../../Component/Header";
 
 import Aos from 'aos'
 import 'aos/dist/aos.css'
+import { FaSistrix } from "react-icons/fa";
 
 
 const Project = () => {
 
+    const [loading, setLoading] = useState(true)
     const [projects,setProject] = useState([])
     console.log(projects)
         useEffect(() =>{
+            setLoading(true)
             fetch('https://my-portfolio-server-psi.vercel.app/projects')
             .then(res =>res.json())
-            .then(data =>setProject(data))
+            .then(data =>{
+                setLoading(false)
+                setProject(data)
+            })
         },[])
         useEffect(() => {
             Aos.init({ duration: 2000 })
           }, [])
+          const [searchText, setSearchText] = useState('')
+       const handleSearch =() =>{
+         setLoading(true)
+         fetch(`https://my-portfolio-server-psi.vercel.app/projectsSearch/${searchText}`)
+         .then(res =>res.json())
+         .then(data =>{
+             setLoading(false)
+            setProject(data)
+             console.log(data)
+         })
+       }
+
+       if(loading){
+        return  <progress className="progress w-56"></progress>
+        }
     return (
-       <div id="project" className=" pb-20" data-aos="zoom-in">
+       <div id="project" className=" pb-20 " data-aos="zoom-in">
+
+<div className="form-control md:w-1/3  absolute z-50 top-32 left-[400px] mx-10 bg-black rounded-2xl">
+                    <div className="">
+                        <input onChange={(event)=>setSearchText(event.target.value)} type="text" placeholder="Search by Technology such as react or javascript" className="input bg-transparent border-cyan-100 w-full pr-16 text-cyan-100"  required/>
+                        <button onClick={handleSearch} className="  bg-transparent hover:none absolute top-2 rounded-2xl right-0 rounded-l-none"><FaSistrix className="w-16 text-cyan-100 h-8"/></button>
+                    </div>
+                </div>
      
         <Header   title={'Projects'}/>
-         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-5 mx-2 ">
+         <div className="mt-20 grid grid-cols-1 md:grid-cols-3 gap-4 py-5 mx-2 ">
         {
-            projects.slice(0,3).map(project =><div  key={project.id} className=" overflow-hidden border-cyan-300 pt-20 hover:pt-0 group  shadow-xl"  style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
+            projects.slice(0,3).map(project =><div  key={project.id} className=" overflow-hidden border-cyan-300 pt-20 hover:pt-0 group  rounded-xl hover:shadow-indigo-600 hover:shadow-xl"  style={{backgroundImage:`url("https://t3.ftcdn.net/jpg/05/12/57/88/360_F_512578835_BdfMwRzEOA0YEih1fn9MZ7EJSNXfsfLM.jpg")`}}>
                <Link to={`/projectDetails/${project._id}`} className="relative"> <img  className="  flex opacity-10 mx-auto   hover:opacity-100 h-1/2  bg-blue-400 transition-all group-hover:w-full group-hover:h-full" src={project.image1} alt=""  title="click here for showing details"/>
                <div>
-               <div className="absolute top-3  opacity-100 group-hover:opacity-0  text-cyan-300  ">
+               <div className="  opacity-100 group-hover:opacity-0  text-cyan-300  ">
                 
                <p className="   text-xl text-center font-bold">{project.projectName}</p>
                <p className=" text-center my-3 border-b-2 pb-2">Technology</p>
